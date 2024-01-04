4f5fcb41a8ff8f9ebf8b4da8b364b29cd76ba9cc
salman <salmanmdsultan@gmail.com>
2024-01-04 21:56:26 +0600

    multiple comment scrape done

--- sentiment_analysis/comment_all.py
+++ sentiment_analysis/comment_all.py
@@ -32,9 +32,15 @@ for k in all_comments:
 
 
 
-for i in range(3):
+for i in range(3,15):
     wait = WebDriverWait(driver,30)
-    comment_element = wait.until(ec.element_to_be_clickable((By.XPATH,'/html/body/div[4]/div/div[9]/div[1]/div[1]/div/div/div/div[3]/div[4]/ul/li[3]/a')))
+    j=str(i)
+    if(i==7):
+        
+        comment_element = wait.until(ec.element_to_be_clickable((By.XPATH,'/html/body/div[4]/div/div[9]/div[1]/div[1]/div/div/div/div[3]/div[4]/ul/li['+j+']/a')))
+    else:
+        comment_element = wait.until(ec.element_to_be_clickable((By.XPATH,'/html/body/div[4]/div/div[9]/div[1]/div[1]/div/div/div/div[3]/div[4]/ul/li[7]/a')))
+    time.sleep(2)
     driver.execute_script('arguments[0].click();',comment_element)
     # driver.find_element(By.CLASS_NAME, 'ant-pagination-item-link').click()
     all_comments =driver.find_elements(By.CLASS_NAME,'review-content-sl')
